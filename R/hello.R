0f64d391de110700d0289c7f09b828efb7fa42a6
mauricio04780 <fumex2004@gmail.com>
2024-07-17 17:44:33 -0500

    first commit

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,4 @@
-output$plotParroq <- renderPlot({
-  ggplot(filtered_data2(), aes(x = Parroquia, y = cont, fill = Parroquia)) +
-    geom_bar(stat = "identity") +
-    theme_minimal() +
-    labs(title = paste("Person Count in", input$province_parroquia), x = "Parroquia", y = "Count of Persons")
-})
+crear_provincia_data <- function(datos) {
+  datos() %>%
+    dplyr::count(Provincia, name = "cantidad_de_personas")
+}
